978ffb5c87d2a530c91a6d965dd766d7dab76978
abdoulkarim18 <toureabdoulkarim145@gmail.com>
2021-12-13 13:12:48 +0000

    add component table-users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('adresse')->nullable();
+            $table->boolean('is_admin')->default(0);
+            $table->boolean('is_deleted')->default(0);
+            $table->boolean('is_verified')->default(0);
+            $table->boolean('is_banned')->default(0);
+            $table->integer('rank')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
