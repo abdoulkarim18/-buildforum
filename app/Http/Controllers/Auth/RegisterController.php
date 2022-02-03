2374acde0809365c9a03ea3364db7b18fe34bf3d
abdoulkarim18 <toureabdoulkarim145@gmail.com>
2022-02-03 17:44:43 +0000

    Integrating Laravel Notifications into our Forum

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Notifications\NewUser;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -69,5 +70,19 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+    }
+
+    protected function redirectTo(){
+
+        $user = auth()->user();
+        $admins = User::where('is_admin', 1)->get();
+        foreach ($admins as $admin) {
+
+            $admin->notify(new NewUser($user));
+        }
+
+        return '/home';
+
     }
 }
